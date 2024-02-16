8631b61ddac0a1f6150f7ee36ca98da5afc8b76f
Stijn Van Horenbeek <stijn.van.horenbeek@outlook.com>
2024-02-16 15:19:51 +0100

    evaluate prefix operators

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -49,7 +49,34 @@ fn eval_expression(expression: &Expression) -> Result<Object, anyhow::Error> {
     match expression {
         Expression::IntegerLiteral(value) => Ok(Object::Integer(*value)),
         Expression::BooleanLiteral(value) => Ok(Object::Boolean(*value)),
+        Expression::Prefix(operator, right) => {
+            let right = eval_expression(right)?;
+            eval_prefix_expression(&operator.to_string(), right)
+        }
         _ => Err(Error::msg("not implemented")),
+    }
+}
+
+fn eval_prefix_expression(operator: &str, right: Object) -> Result<Object, anyhow::Error> {
+    match operator {
+        "!" => eval_bang_operator_expression(right),
+        "-" => eval_minus_prefix_operator_expression(right),
+        _ => Err(Error::msg("unknown operator")),
+    }
+}
+
+fn eval_bang_operator_expression(right: Object) -> Result<Object, anyhow::Error> {
+    match right {
+        Object::Boolean(value) => Ok(Object::Boolean(!value)),
+        Object::Null => Ok(Object::Boolean(true)),
+        _ => Ok(Object::Boolean(false)),
+    }
+}
+
+fn eval_minus_prefix_operator_expression(right: Object) -> Result<Object, anyhow::Error> {
+    match right {
+        Object::Integer(value) => Ok(Object::Integer(-value)),
+        _ => Ok(Object::Null),
     }
 }
 
@@ -61,11 +88,36 @@ mod tests {
 
     #[test]
     fn test_eval_integer_expression() {
-        let tests = vec![("5", 5), ("10", 10)];
+        let tests = vec![("5", 5), ("10", 10), ("-5", -5), ("-10", -10)];
 
         for (input, expected) in tests {
             let evaluated = test_eval(input);
             test_integer_object(evaluated, expected);
+        }
+    }
+
+    #[test]
+    fn test_eval_boolean_expression() {
+        let tests = vec![("true", true), ("false", false)];
+
+        for (input, expected) in tests {
+            let evaluated = test_eval(input);
+            test_boolean_object(evaluated, expected);
+        }
+    }
+
+    #[test]
+    fn test_bang_operator() {
+        let tests = vec![
+            ("!true", false),
+            ("!false", true),
+            ("!5", false),
+            ("!!true", true),
+        ];
+
+        for (input, expected) in tests {
+            let evaluated = test_eval(input);
+            test_boolean_object(evaluated, expected);
         }
     }
 
@@ -84,6 +136,17 @@ mod tests {
             }
             _ => {
                 panic!("Object is not Integer. got={}", obj);
+            }
+        }
+    }
+
+    fn test_boolean_object(obj: Object, expected: bool) {
+        match obj {
+            Object::Boolean(value) => {
+                assert_eq!(value, expected);
+            }
+            _ => {
+                panic!("Object is not Boolean. got={}", obj);
             }
         }
     }
